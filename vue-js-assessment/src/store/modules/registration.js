5f2d1e53e6eb0d3d1508a1a264d79eac056c4055
ZanJereb <zan_jereb@hotmail.com>
2023-10-20 14:48:45 +0200

    Fixed error message issue

--- vue-js-assessment/src/store/modules/registration.js
+++ vue-js-assessment/src/store/modules/registration.js
@@ -8,16 +8,21 @@ const mutations = {
   SET_ERROR_MSG(state, errorMessage) {
     state.errorMessage += errorMessage;
   },
+  RESET_ERROR_MSG(state) {
+    state.errorMessage = '';
+  },
 };
 
 const actions = {
   createUser({ commit }, userData) {
     commit('CHANGE_STATUS', 'Submited');
     commit('SET_USER_DATA', userData);
+    commit('RESET_ERROR_MSG');
   },
   createError({ commit }, error) {
+    commit('RESET_ERROR_MSG');
     commit('CHANGE_STATUS', 'Error');
-    commit('SET_ERROR_MSG', error);
+    commit('SET_ERROR_MSG', `Error: ${error}`);
   },
 };
 
@@ -28,7 +33,7 @@ const state = {
     email: '',
     phoneNumber: '',
   },
-  errorMessage: 'Error: ',
+  errorMessage: '',
 };
 
 export default {
