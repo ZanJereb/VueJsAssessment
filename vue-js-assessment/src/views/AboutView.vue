d1f04c32c026b294fdfd830c3ca8b05c454a3552
ZanJereb <zan_jereb@hotmail.com>
2023-10-21 18:26:20 +0200

    update

--- vue-js-assessment/src/views/AboutView.vue
+++ vue-js-assessment/src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>This is an about page</h1>
+    <h1>This is an about page with custom directives</h1>
     <p v-highlight="'yellow'">Hover over this text to highlight it in yellow.</p>
     <p v-highlight="'lightblue'">Hover over this text to highlight it in light blue.</p>
   </div>
